b0f727e133b0cdb7778cfdc031d7a253e8a5abbd
bailiao <3190105008@zju.edu.cn>
2021-11-18 01:08:40 +0800

    Update models.py
    用户数据表

--- code/SRE/register/models.py
+++ code/SRE/register/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    UID = models.UUIDField(max_length=15, primary_key=True, blank = False)
+    Name = models.CharField(max_length=10, blank=False)
+    Password = models.CharField(max_length=20, blank=False)
+    Email = models.EmailField(max_length=30)
+    Github = models.CharField(max_length=30)
+
+
+    # def save(self, *args, **kwargs):
+    #     md5 = hashlib.md5()
+    #     md5.update(self.password.encode())
+    #     self.password = md5.hexdigest()
+    #     super(Student, self).save(*args, **kwargs)
