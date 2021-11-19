b6add59369be78f2c04c84c60bef5db341cd6957
jiyujia926 <84773677+jiyujia926@users.noreply.github.com>
2021-11-19 19:30:57 +0800

    完善并修复issue爬虫的一些bug

--- code/爬虫实验/get_issue.py
+++ code/爬虫实验/get_issue.py
@@ -43,9 +43,12 @@ def get_open_issue(rawurl:str):
         
         
     for item in soup.find_all('a'):
-        link = str(item['href'])
-        if link.find(checkstr)==1 and link.count('/')==4:
+        link = "http://github.com"+str(item['href'])
+        if link.find(checkstr)==18 and link.count('/')==6:
             # print(link)
+            pinned = str(item['class'])
+            if pinned.find('Link')>=0:
+                continue
             if link in issuelist:
                 continue
             else:
@@ -78,9 +81,12 @@ def get_open_issue(rawurl:str):
             next_page_url = "http://github.com"+next_page_url[indexstart+6:indexend-2]
             # print(next_page_url)
         for item in soup.find_all('a'):
-            link = str(item['href'])
-            if link.find(checkstr)==1 and link.count('/')==4:
+            link = "http://github.com"+str(item['href'])
+            if link.find(checkstr)==18 and link.count('/')==6:
                 # print(link)
+                pinned = str(item['class'])
+                if pinned.find('Link')>=0:
+                    continue
                 if link in issuelist:
                     continue
                 else:
@@ -88,6 +94,99 @@ def get_open_issue(rawurl:str):
     print(issuelist)      
     print(len(issuelist)) 
 
+
+def get_closed_issue(rawurl:str):
+    issuelist=[]
+    url = ""
+    if rawurl[-1] == '/':
+        url = rawurl + "issues?q=is%3Aissue+is%3Aclosed"
+    else:
+        url = rawurl +"/issues?q=is%3Aissue+is%3Aclosed"
+    indexstart = url.find('com')
+    indexend = url.find('issue')
+    checkstr = url[indexstart+4:indexend]+"issues"
+    print(checkstr)
+    strhtml = requests.get(url)
+    soup = BeautifulSoup(strhtml.text,'lxml')
+    has_next_page = 0
+    next_page_url_list=[]
+    next_page_url = ""
+    for item in soup.find_all('a'):
+        rawa = str(item)
+        if rawa.find('Next')>=0:
+            # print(rawa)
+            if rawa not in next_page_url_list:
+                next_page_url_list.append(rawa)
+            has_next_page = 1
+    for item in soup.find_all('span'):
+        rawstr = str(item)
+        if rawstr.find('next_page') >=0:
+            # print(item)
+            has_next_page = 0
+    # print(has_next_page)           
+    if has_next_page:
+        # print(next_page_url_list)
+        next_page_url = str(next_page_url_list[0])
+        indexstart = next_page_url.find('href')
+        indexend = next_page_url.find('rel')
+        next_page_url = "http://github.com"+next_page_url[indexstart+6:indexend-2]
+        # print(next_page_url)
+        
+        
+    for item in soup.find_all('a'):
+        link = "http://github.com"+str(item['href'])
+        if link.find(checkstr)==18 and link.count('/')==6:
+            # print(link)
+            pinned = str(item['class'])
+            if pinned.find("Link")>=0:
+                continue
+            if link in issuelist:
+                continue
+            else:
+                issuelist.append(link)
+                
+    while (has_next_page):
+        strhtml = requests.get(next_page_url)
+        soup = BeautifulSoup(strhtml.text,'lxml')
+        has_next_page = 0
+        next_page_url_list=[]
+        next_page_url=""
+        for item in soup.find_all('a'):
+            rawa = str(item)
+            if rawa.find('Next')>=0:
+                # print(rawa)
+                if rawa not in next_page_url_list:
+                    next_page_url_list.append(rawa)
+                has_next_page = 1
+        for item in soup.find_all('span'):
+            rawstr = str(item)
+            if rawstr.find('next_page') >=0:
+            # print(item)
+                has_next_page = 0
+        # print(has_next_page)
+        if has_next_page:
+            # print(next_page_url_list)
+            next_page_url = str(next_page_url_list[0])
+            indexstart = next_page_url.find('href')
+            indexend = next_page_url.find('rel')
+            next_page_url = "http://github.com"+next_page_url[indexstart+6:indexend-2]
+            # print(next_page_url)
+        for item in soup.find_all('a'):
+            link = "http://github.com"+str(item['href'])
+            if link.find(checkstr)==18 and link.count('/')==6:
+                # print(link)
+                pinned = str(item['class'])
+                if pinned.find("Link")>=0:
+                    continue
+                if link in issuelist:
+                    continue
+                else:
+                    issuelist.append(link)
+    print(issuelist)    
+    print(len(issuelist))
+
+
 if __name__ == "__main__":
     url = input()
     get_open_issue(url)
+    get_closed_issue(url)
