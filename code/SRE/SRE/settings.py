ff180ab144ec89d42fa430dfa9b51e1a2a1500e8
jiyujia926 <84773677+jiyujia926@users.noreply.github.com>
2021-11-21 03:31:34 +0800

    修复bug

--- code/SRE/SRE/settings.py
+++ code/SRE/SRE/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'server.middlewares.MyTest'
+    # 'server.middlewares.MyTest'
 ]
 
 ROOT_URLCONF = 'SRE.urls'
