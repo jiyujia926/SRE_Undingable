cb3233ddb715a534d3c3c6203db14921d3e4d155
jiyujia926 <84773677+jiyujia926@users.noreply.github.com>
2021-12-16 16:31:05 +0800

    Update tasks.py

--- code/SRE/dashboard/tasks.py
+++ code/SRE/dashboard/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .get_commit import getcommit
-from .get_issue import open_issue_time,closed_issue_time
+from .get_issue import get_open_issue,get_closed_issue
 from . import models
 import django
 django.setup()  					# 前4句引入django测试环境
@@ -33,7 +33,7 @@ def analyze_commit(url:str):
 
 @shared_task
 def analyze_open_issue(url:str):
-    openissuelist = open_issue_time(url)
+    openissuelist = get_open_issue(url)
     project = models.Project.objects.filter(RepositoryURL=url).first()
     if openissuelist:
         for item in openissuelist:
@@ -52,7 +52,7 @@ def analyze_open_issue(url:str):
         
 @shared_task
 def analyze_close_issue(url:str):
-    closeissuelist = closed_issue_time(url)
+    closeissuelist = get_closed_issue(url)
     project = models.Project.objects.filter(RepositoryURL=url).first()
     if closeissuelist:
         for item in closeissuelist:
