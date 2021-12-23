74b460635da3c1104cc10c2df8166c23047b64b3
Wu Xinberry <3190103044@zju.edu.cn>
2021-12-24 01:01:33 +0800

    修改PieChart组件，实现双环饼图 吴

--- code/front-end/src/components/Charts/PieChart.js
+++ code/front-end/src/components/Charts/PieChart.js
@@ -6,6 +6,24 @@ import PropTypes from "prop-types";
 const PieChart = (props) => {
   const { data } = props;
   let id = nanoid();
+  let series_items = [];
+  let cnt = 0;
+  for (let item in data) {
+    let tmp_item = {};
+    tmp_item.name = item.repoName;
+    tmp_item.type = "pie";
+    tmp_item.label = { show: false, position: "center" };
+    tmp_item.itemStyle = { borderRadius: 2 };
+    tmp_item.data = item.data;
+    if (cnt == 0) {
+      tmp_item.radius = ["80%", "100%"];
+      cnt++;
+    } else {
+      tmp_item.radius = ["50%", "70%"];
+    }
+    series_items.push(tmp_item);
+  }
+
   const initChart = () => {
     let element = document.getElementById(id);
     let myChart = eCharts.init(element);
@@ -23,19 +41,7 @@ const PieChart = (props) => {
         },
         bottom: "-2%",
       },
-      series: [
-        {
-          name: "Nightingale Chart",
-          type: "pie",
-          radius: [10, 60],
-          center: ["50%", "50%"],
-          roseType: "area",
-          itemStyle: {
-            borderRadius: 2,
-          },
-          data: data,
-        },
-      ],
+      series: series_items,
     };
     option && myChart.setOption(option, true);
     window.addEventListener("resize", function () {
